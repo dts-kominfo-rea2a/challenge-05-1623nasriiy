82b205f7333a02521f863a0153ad8dc580be18f1
Irwan-Suhendra <ierwanz@gmail.com>
2022-06-05 22:49:19 +0700

    challenge-05

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (list, callback) => {
+  let arr = callback(list);
+  let arr2 = [];
+  for (let i = 0; i < arr.length; i++)  {
+    arr2.push(i+1 + '.'+arr[i])
+  } return arr2
+}
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (datarray) =>{
+  return datarray.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (datarray)=> {
+  return datarray.sort().reverse()
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
